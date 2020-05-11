b42cae3a118f99e83801ef902031c4a9c46e6d64
zacht3000 <56853262+SantiagoNaranjoMarcillo@users.noreply.github.com>
2020-05-11 16:59:39 +0200

    Modicicado el nombre y añadido los get a los usuarios.

---
+++ src/java/clientes/TipoUsuario.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientes;
+
+/**
+ *
+ * @author Santiago Naranjo Marcillo
+ */
+public enum TipoUsuario {
+    NORMAL, ADMINISTRADOR; 
+    
+    
+}

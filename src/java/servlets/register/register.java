5f867ebb6073213189ead8c10437e90dff7c4522
zacht3000 <56853262+SantiagoNaranjoMarcillo@users.noreply.github.com>
2020-05-13 00:35:38 +0200

    Register terminado

---
+++ src/java/servlets/register/register.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets.register;
+
+import Exception.UserAlreadyExistsException;
+import Exception.AttributesRequirementsException;
+import Exception.UserRegisterException;
+import usuarios.ListaUsuarios;
+import usuarios.Usuario;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.regex.Pattern;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import usuarios.TipoUsuario;
+
+/**
+ *
+ * @author Santiago Naranjo Marcillo
+ */
+public class register extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        ServletContext application = getServletContext();
+        ListaUsuarios usuariosRegistrados =  (ListaUsuarios) application.getAttribute("usuariosRegistrados");
+        try {
+            String nickname = getParameter(request, "nickname", usuariosRegistrados);
+            String nombre = getParameter(request, "nombre", usuariosRegistrados);
+            String apellidos = getParameter(request, "apellidos", usuariosRegistrados);
+            String correo = getParameter(request, "correo", usuariosRegistrados);
+            String contrasenya = getParameter(request, "contrasenya", usuariosRegistrados);
+            
+            usuariosRegistrados.mete(new Usuario(nickname, correo, nombre, apellidos, contrasenya, TipoUsuario.NORMAL));
+            request.setAttribute("messageCorrect", "Registrado correctamente.");
+            application.getRequestDispatcher("/html/login.jsp").forward(request, response);
+        } catch (UserRegisterException | AttributesRequirementsException | UserAlreadyExistsException ex) {
+            request.setAttribute("messageError", ex.getMessage());
+            application.getRequestDispatcher("/html/register.jsp").forward(request, response);
+        } catch (SQLException ex) {
+            // Mandamos el mensaje de error al jsp donde se mostrará
+            request.setAttribute("messageError", "Error al consultar la base de datos. Inténtelo más tarde");
+            application.getRequestDispatcher("/html/register.jsp").forward(request, response);
+        }
+        
+    }
+    
+    private String getParameter(HttpServletRequest request, String texto, ListaUsuarios usuariosRegistrados) 
+            throws UserRegisterException, AttributesRequirementsException, UserAlreadyExistsException, SQLException{
+        String parameter = request.getParameter(texto);
+        
+        if(parameter.isEmpty()) {
+            throw new UserRegisterException(texto);
+        }
+        
+        if(texto.equals("correo")){
+            parameter = getCorreo(texto, parameter);
+            if(usuariosRegistrados.yaExisteCorreo(parameter)){
+                throw new UserAlreadyExistsException(texto);
+            }
+        } else if(texto.equals("nickname")){
+            parameter = getNickName(texto, parameter);
+            if(usuariosRegistrados.yaExisteNickName(parameter)){
+                throw new UserAlreadyExistsException(texto);
+            }
+        } else if(texto.equals("contrasyna")){
+            parameter = getContrasenya(texto, parameter);
+        }
+        
+        return parameter;
+    }
+    
+    private String getCorreo(String texto, String parameter) throws AttributesRequirementsException{
+        
+        if (!Pattern.matches(Usuario.PATRON_CORREO, parameter)) {
+            throw new AttributesRequirementsException(texto);
+        }
+        
+        return parameter;   
+    }
+    
+    private String getNickName(String texto, String parameter) throws AttributesRequirementsException{
+        
+        if (!Pattern.matches(Usuario.PATRON_NICKNAME, parameter)) {
+            throw new AttributesRequirementsException(texto);
+        }
+        
+        return parameter;   
+    }
+    
+    private String getContrasenya(String texto, String parameter) throws AttributesRequirementsException{
+        
+        if (!Pattern.matches(Usuario.PATRON_CONTRASENYA, parameter)) {
+            throw new AttributesRequirementsException(texto);
+        }
+        
+        return parameter;   
+    }
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}

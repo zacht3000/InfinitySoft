6f2a23c0069e7b28e5efa1857b2292ddd17621c7
zacht3000 <56853262+SantiagoNaranjoMarcillo@users.noreply.github.com>
2020-05-22 02:25:42 +0200

    Añadido test para comprobar los datos vacios del nombre o correo.

--- test/seleniumtests/tests/html/registerTest.java
+++ test/seleniumtests/tests/html/registerTest.java
@@ -5,10 +5,33 @@
  */
 package seleniumtests.tests.html;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import seleniumtests.SeleniumTest;
+import seleniumtests.tests.index.IndexTest;
+
 /**
  *
  * @author Santiago Naranjo Marcillo
  */
-public class registerTest {
+public class registerTest extends SeleniumTest{
     
+    @Before
+    public void accederLinkDeRegister(){
+        clickAndWait(driver.findElement(By.id("register"))); 
+    }
+    
+    @Test
+    public void testTittle() {
+        assertEquals("INFINITYSOFT", driver.getTitle());
+    }
+    
+    @Test
+    public void testComprobarDatoVacioNombreCorreo() {
+        clickAndWait(driver.findElement(By.id("send")));
+        assertEquals("Introduce tu nombre de usuario.", driver.findElement(By.id("error")).getText());
+    }
 }

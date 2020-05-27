56c2ae040c7d5e8ce75c56ec65227d50a3bd0359
zacht3000 <56853262+SantiagoNaranjoMarcillo@users.noreply.github.com>
2020-05-27 03:12:47 +0200

    Añadida opción para terminar editar el usuario.

--- src/java/servlets/updateUser/UpdateUser.java
+++ src/java/servlets/updateUser/UpdateUser.java
@@ -3,27 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package servlets.manageUsers;
+package servlets.updateUser;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import main.Exception.UserNotExistException;
-import main.Exception.UserRegisterException;
-import main.usuarios.ListaUsuarios;
-import main.usuarios.Usuario;
 
 /**
  *
  * @author Santiago Naranjo Marcillo
  */
-public class ManageUsers extends HttpServlet {
+public class UpdateUser extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -37,30 +30,18 @@ public class ManageUsers extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        ServletContext application = getServletContext();
-        ListaUsuarios usuariosRegistrados = (ListaUsuarios) application.getAttribute("usuariosRegistrados");
-        
-        try {
-            String nombre = getParameter(request, "manageUser", "usuario");
-            application.setAttribute("updateUser", usuariosRegistrados.getBuscaUsuarioNickNameOrCorreo(nombre));
-            response.sendRedirect("/InfinitySoft/html/updateUser.jsp");
-        } catch (UserRegisterException | UserNotExistException ex) {
-            request.setAttribute("messageError", ex.getMessage());
-            application.getRequestDispatcher("/html/manageUsers.jsp").forward(request, response);
-        }catch (SQLException ex) {
-            request.setAttribute("messageError", ex.getMessage());
-            application.getRequestDispatcher("/html/manageUsers.jsp").forward(request, response);
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UpdateUser</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UpdateUser at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
-        
-    }
-    
-    private String getParameter(HttpServletRequest request, String param, String texto) throws UserRegisterException {
-        String parameter = request.getParameter(param);
-        
-        if (parameter.isEmpty()) {
-            throw new UserRegisterException(texto);
-        }
-        return parameter;
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -101,7 +82,5 @@ public class ManageUsers extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
-    
 
 }

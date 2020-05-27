3d83646333cbabb249e0afd6519d73a2a99a5cac
zacht3000 <56853262+SantiagoNaranjoMarcillo@users.noreply.github.com>
2020-05-27 12:01:34 +0200

    Creado metodo para actualizar los datos de los usaurios.

--- src/java/main/usuarios/ListaUsuarios.java
+++ src/java/main/usuarios/ListaUsuarios.java
@@ -22,6 +22,12 @@ import main.Exception.UserNotExistException;
 public class ListaUsuarios {
     
     private static final String NOMBRE_TABLA = "USUARIO";
+    private static final String NOMBRE_COLUMNA_NICKNAME = "NICKNAME";
+    private static final String NOMBRE_COLUMNA_CORREO = "CORREO";
+    private static final String NOMBRE_COLUMNA_NOMBRE = "NOMBRE";
+    private static final String NOMBRE_COLUMNA_APELLIDOS = "APELLIDOS";
+    private static final String NOMBRE_COLUMNA_CONTRASENYA = "CONTRASENYA";
+    private static final String NOMBRE_COLUMNA_TIPO = "TIPO";
     private DataSource dataSource;
     private ArrayList<Usuario> usuarios;
     
@@ -93,6 +99,30 @@ public class ListaUsuarios {
     
     public boolean listaUsuarioNoAdminAnonimo() throws SQLException{
         return getUsuarios().size() <= 2;
+    }
+
+    public void modificarUsuario(Usuario usuario) throws SQLException {
+        String sentenciaSQL = "UPDATE " + NOMBRE_TABLA + " SET " 
+                + NOMBRE_COLUMNA_NICKNAME + " = '"
+                + usuario.getNickName() + "', "
+                + NOMBRE_COLUMNA_CORREO + " = '"
+                + usuario.getCorreo() + "', "
+                + NOMBRE_COLUMNA_NOMBRE + " = '"
+                + usuario.getNombre() + "', "
+                + NOMBRE_COLUMNA_APELLIDOS + " = '"
+                + usuario.getApellidos() + "', "
+                + NOMBRE_COLUMNA_CONTRASENYA+ " = '"
+                + usuario.getContrasenya() + "', "
+                + NOMBRE_COLUMNA_TIPO + " = '"
+                + usuario.getTipo().toString() + "' WHERE " 
+                + NOMBRE_COLUMNA_NICKNAME + " = '"
+                + usuario.getNickName() + "'";
+
+        try (Connection connection = this.dataSource.getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(sentenciaSQL);
+            }
+        }
     }
 
     
